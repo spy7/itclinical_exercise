b94d7c7864ecb0712493fdfda7a6d0ac6efec81e
Carlos <carlosspy@gmail.com>
2022-01-22 11:41:45 -0300

    Refactor method

--- src/main/java/com/itclinical/exercise/service/ExerciseService.java
+++ src/main/java/com/itclinical/exercise/service/ExerciseService.java
@@ -5,6 +5,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class ExerciseService {
     public String getUpper(String input, int n) {
-        return "ITCLNA";
+        int length = input.length();
+        char ch;
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < length; i+=n) {
+            ch = input.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                result.append(ch);
+            }
+        }
+        
+        return result.toString();
     }
 }
