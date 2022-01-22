8f262388fc3ddf6a160690dd3dcbc71061fdc778
Carlos <carlosspy@gmail.com>
2022-01-22 10:58:26 -0300

    Creating service to pass the test

---
+++ src/main/java/com/itclinical/exercise/service/ExerciseService.java
@@ -0,0 +1,10 @@
+package com.itclinical.exercise.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExerciseService {
+    public String getUpper(String input, int n) {
+        return "ITCLNA";
+    }
+}
